c622fb78cdf18e0cb27393a33ad488ed268705e2
Benjamin GONON <bgonon@homiwoo.com>
2025-02-07 12:03:53 +0100

    add vega in angular side

--- ng-19/src/app/app.component.ts
+++ ng-19/src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { ChartComponent } from "./chart/chart.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: "app-root",
+  imports: [RouterOutlet, ChartComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  title = 'ng-19';
+  title = "ng-19";
 }
